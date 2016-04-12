74d651103a00d4171c470ba83c83127d31fc10d3
andrewtholt <andrewtholt60@gmail.com>
2016-04-12 13:23:40 +0100

    Drop tables, if they exist, crate afressh and set a default MQTT server

--- SocketServer/setup.sql
+++ SocketServer/setup.sql
@@ -1,10 +1,23 @@
+drop table if exists mqtt_settings ;
+drop table if exists mqtt_map ;
 
+-- 
+-- can have multiplke server for load blancing or#
+-- redundancy
+--
 create table mqtt_settings (
-    address varchar not null default "127.0.0.1",
-    port int not null default 1883
+    id integer primary key autoincrement,
+    address varchar default "127.0.0.1",
+    port int default 1883
 );
 
 create table mqtt_map (
     name varchar,
     path varchar
-)
+);
+
+-- 
+-- Create default entry for MQTT server
+--
+insert into mqtt_settings (id) values (1);
+
