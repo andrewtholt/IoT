a87b07697f191457a4aaad4306243d980eafdae8
andrewtholt <andrewtholt60@gmail.com>
2016-04-12 13:29:03 +0100

    Added indices.

--- SocketServer/setup.sql
+++ SocketServer/setup.sql
@@ -1,6 +1,9 @@
 drop table if exists mqtt_settings ;
 drop table if exists mqtt_map ;
 
+drop index if exists mqtt_map_forward ;
+drop index if exists mqtt_map_reverse ;
+
 -- 
 -- can have multiplke server for load blancing or#
 -- redundancy
@@ -11,10 +14,17 @@ create table mqtt_settings (
     port int default 1883
 );
 
+-- hold a shortahand version of the long path,
+-- this assists device that have a small amount of
+-- RAM in which to assemble messages.
+--
 create table mqtt_map (
     name varchar,
     path varchar
 );
+
+create index mqtt_map_forward on mqtt_map(name);
+create index mqtt_map_reverse on mqtt_map(path);
 
 -- 
 -- Create default entry for MQTT server
